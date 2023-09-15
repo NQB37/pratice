b3b9825457a247b3926d8dc4c1a1664b78ba5350
nquocbao37 <nquocbao37@gmail.com>
2023-09-15 23:26:45 +0200

    update JS for hamburgerNav project
    change color of navbar when click

--- hamburgerNav/main.js
+++ hamburgerNav/main.js
@@ -1,5 +1,13 @@
 let icon = document.querySelector(".header__btn__icon"),
-    menu = document.querySelector("#menu");
+    menu = document.querySelector("#menu"),
+    liEle = document.querySelectorAll("li a");
+
+liEle.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector("li a.active").classList.remove("active");
+        btn.classList.add("active");
+    });
+});
 
 icon.addEventListener("click", () => {
     menu.classList.toggle("open");
